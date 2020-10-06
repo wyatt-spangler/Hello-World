e412b1b201cd413c01a9b9d59807fca5fd48f090
Wyatt Spangler <wyatt.spa@outlook.com>
2020-10-06 08:47:51 -0400

    Add project files.

---
+++ Hello World/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GettingInput
+{
+    class Program
+    {
+        static void Main()
+        {
+            // asked you how old you are
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old!");
+        }
+    }
+}
